54b9313a9c20a516f235e3fc20103300b4bb91be
LeJacque Gillespie <m0321us@yahoo.com>
2016-07-04 10:50:07 -0500

    Initial commit

---
+++ app/src/main/java/g620apps/circlebuttons/CircleButtonsMainActivity.java
@@ -0,0 +1,14 @@
+package g620apps.circlebuttons;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CircleButtonsMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circle_buttons_main);
+
+    }
+}
